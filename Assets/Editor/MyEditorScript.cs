7aef6ceb4b92590ee36c99667bb11640f825f9dc
Chris K <ck@gearworks.dk>
2014-03-25 15:30:49 +0100

    small fix v2

--- Assets/Editor/MyEditorScript.cs
+++ Assets/Editor/MyEditorScript.cs
@@ -7,7 +7,7 @@ class MyEditorScript {
 	static string[] SCENES = FindEnabledEditorScenes();
 	
 	static string APP_NAME = "JenkinsUnityTest01";
-	static string TARGET_DIR = "target";
+	static string TARGET_DIR = "/Users/Shared/Jenkins/Desktop/Test";
 	
 	[MenuItem ("Custom/CI/Build Mac OS X")]
 	static void PerformMacOSXBuild ()
