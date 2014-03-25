d5e6f10d847fbb5289a577cf8c57ac0646d87466
christropicana <ck@gearworks.dk>
2014-03-25 01:38:57 +0100

    Editor Script for Jeninks

---
+++ Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+class MyEditorScript {
+	static string[] SCENES = FindEnabledEditorScenes();
+	
+	static string APP_NAME = "JenkinsUnityTest01";
+	static string TARGET_DIR = "Users/christropicana/Desktop/Tests";
+	
+	[MenuItem ("Custom/CI/Build Mac OS X")]
+	static void PerformMacOSXBuild ()
+	{
+		string target_dir = APP_NAME + ".app";
+		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneOSXIntel,BuildOptions.None);
+	}
+	
+	private static string[] FindEnabledEditorScenes() {
+		List<string> EditorScenes = new List<string>();
+		foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+			if (!scene.enabled) continue;
+			EditorScenes.Add(scene.path);
+		}
+		return EditorScenes.ToArray();
+	}
+	
+	static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+		string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
+		if (res.Length > 0) {
+			throw new System.Exception("BuildPlayer failure: " + res);
+		}
+	}
+}
